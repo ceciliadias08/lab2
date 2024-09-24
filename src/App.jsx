0c2931ce14ae0c1e471aae3fcaba166d3f5e488a
ceciliadias08 <ceciliadias08@hotmail.com>
2024-09-24 15:11:24 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,51 @@ function App() {
 
   return (
     <>
-      <h1>Hello world!</h1>
-      <Resorts />
+      <h1>Resorts Lite</h1>
+      <div className="container">
+        <Resorts
+          name="Indonesia"
+          hotel="Gili Air Hotel"
+          image="\public\1.jpg"
+          rate="4.8 ★"
+          price="$589/night"
+        />
+        <Resorts
+          name="Seychelles"
+          hotel="Hilton Resort"
+          image="\public\2.jpg"
+          rate="4.2 ★"
+          price="$629/night"
+        />
+        <Resorts
+          name="US Virgin Islands"
+          hotel="Goa Resort"
+          image="\public\3.jpg"
+          rate="3.5 ★"
+          price="$485/night"
+        />
+        <Resorts
+          name="Bahamas"
+          hotel="Kuredu Resort"
+          image="\public\4.jpg"
+          rate="4.1 ★"
+          price="$729/night"
+        />
+        <Resorts
+          name="Mauritius"
+          hotel="Trou D'eau Douce"
+          image="\public\5.jpg"
+          rate="4.9 ★"
+          price="$877/night"
+        />
+        <Resorts
+          name="Bermuda"
+          hotel="Staniel Cay Hotel"
+          image="\public\6.jpg"
+          rate="3.2 ★"
+          price="$365/night"
+        />
+      </div>
     </>
   );
 }
--- src/Components/Resorts.jsx
+++ src/Components/Resorts.jsx
@@ -1,3 +1,14 @@
-export default function Resort() {
-  return <h1>Resorts Lite</h1>;
+export default function Resort(props) {
+  console.log(props);
+  return (
+    <body>
+      <div className="card">
+        <img className="image" src={props.image} width="200px"></img>
+        <h2>{props.name}</h2>
+        <p>{props.hotel}</p>
+        <h3 className="green">{props.rate}</h3>
+        <p>{props.price}</p>
+      </div>
+    </body>
+  );
 }
